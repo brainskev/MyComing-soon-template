514aa2bbd69be1dfc97114b501788d1ae9d4112f
kelvin <ngotho.kelvin@yahoo.com>
2024-03-03 23:57:45 +0300

    initial commit

---
+++ data/data.ts
@@ -0,0 +1,22 @@
+const data = {
+    sitename: "Rascom ltd",
+    sitetagline: "Unlocking Dreams, One Home at a Time",
+    sitelogo: "",
+    title: "Comming Soon",
+    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 🗓️. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    newsletterheading: "Stay tuned for our live updates!",
+    copyrightText: 'Copyright © 2024 Rascon ltd',
+    socialIconsHeading: "Follow Us 📣",
+    socialIcons: [
+        {
+            icon: "facebook",
+            link: "https://www.facebook.com/rascomproperties/",
+        },
+        {
+            icon: "twitter",
+            link: "https://www.facebook.com/rascomproperties/",
+        },
+    ],
+}
+
+export default data;
