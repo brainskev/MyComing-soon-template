7392dfbbde2545dc17f0aa15027720a0f40b9479
kelvin <ngotho.kelvin@yahoo.com>
2024-03-04 00:03:37 +0300

    Add comment

--- data/data.ts
+++ data/data.ts
@@ -3,7 +3,7 @@ const data = {
     sitetagline: "Unlocking Dreams, One Home at a Time",
     sitelogo: "",
     title: "Comming Soon",
-    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 🗓️. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 🗓️. Our team is putting the finishing touches on our new website 🚀 that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
     copyrightText: 'Copyright © 2024 Rascon ltd',
     socialIconsHeading: "Follow Us 📣",
